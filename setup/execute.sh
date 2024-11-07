6227d896b4a740f57ce6c5c6d52ff06bf533ce00
Henok Seifu <henok.seifu@cozero.io>
2024-11-07 08:58:59 +0100

    fix: Uses local MySQL database

---
+++ setup/execute.sh
@@ -0,0 +1,10 @@
+docker run \
+    --name traccar \
+    --hostname traccar \
+    --detach --restart unless-stopped \
+    --publish 80:8082 \
+    --publish 5000-5150:5000-5150 \
+    --publish 5000-5150:5000-5150/udp \
+    --volume ~/opt/traccar/logs:/opt/traccar/logs:rw \
+    --volume ~/opt/traccar/traccar.xml:/opt/traccar/conf/traccar.xml:ro \
+    henokujuzi/enroute:latest
